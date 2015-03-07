a20ad6ab36b6b21bb1ba5d2603c57048a31e36ca
Joshua Ball <joshball@yahoo-inc.com>
2015-03-06 20:14:20 -0800

    Polymorphic

---
+++ Polymorphic.hs
@@ -0,0 +1,22 @@
+module Polymorphic where
+
+type Sym = String
+
+data Expr
+    = Var Sym
+    | App Expr Expr
+    | Lam Sym Type Expr
+    | TLam Sym Kind Expr
+    | TApp Expr Type
+    deriving (Eq, Read, Show)
+
+data Type
+    = Arrow Type Type
+    | Base
+    | TVar Sym
+    deriving (Eq, Read, Show)
+
+data Kind
+    = KArrow Type Type
+    | Star
+    deriving (Eq, Read, Show)
